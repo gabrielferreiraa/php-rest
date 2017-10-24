53dd561aca39a0034475b8e2239e11da32e7d223
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2017-10-24 18:12:25 -0200

    creating trait interface to rest

---
+++ plugins/Ws/src/Controller/Traits/CrudTrait.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Ws\Controller\Traits;
+
+use Cake\ORM\TableRegistry;
+
+trait CrudTrait
+{
+    protected $Entity = null;
+    protected $EntityContains = null;
+    protected $Label = 'registro';
+
+    public function setEntity($entity)
+    {
+        $this->Entity = TableRegistry::get($entity);
+    }
+
+    public function setEntityContains($contains)
+    {
+        $this->EntityContains = $contains;
+    }
+
+    public function setLabel($label)
+    {
+        $this->Label = $label;
+    }
+
+    public function getEntity()
+    {
+        return $this->Entity;
+    }
+
+    public function getEntityContains()
+    {
+        return $this->EntityContains;
+    }
+
+    public function getLabel()
+    {
+        return $this->Label;
+    }
+
+    public function add()
+    {
+        $response = [
+            'status' => 'error',
+            'message' => "Problema ao salvar {$this->getLabel()}, por favor tente novamente",
+            'data' => []
+        ];
+
+        if ($this->request->is('post')) {
+            $TableEntity = $this->getEntity();
+            $post = $this->request->getData();
+
+            $entity = $TableEntity->newEntity();
+            $entity = $TableEntity->patchEntity($entity, $post);
+
+            if ($TableEntity->save($entity)) {
+                $response = [
+                    'status' => 'success',
+                    'message' => ucfirst($this->getLabel()) . ' salva com sucesso',
+                    'data' => $entity
+                ];
+            }
+        }
+
+        $this->set(compact('response'));
+        $this->set('_serialize', ['response']);
+    }
+
+    public function edit($id = null)
+    {
+        $EntityTable = $this->getEntity();
+
+        $entity = $EntityTable->find();
+
+        if ($this->getEntityContains()) {
+            $entity->contain($this->getEntityContains());
+        }
+
+        $entity = $entity
+            ->where([
+                "{$EntityTable->alias()}.id" => $id
+            ])
+            ->first();
+
+        $hasRegister = !empty($entity);
+
+        $response = [
+            'status' => 'error',
+            'message' => "Problema ao editar {$this->getLabel()}, por favor tente novamente",
+            'data' => []
+        ];
+
+        if ($hasRegister) {
+            if ($this->request->is(['patch', 'post', 'put'])) {
+                $post = $this->request->getData();
+
+                $entity = $EntityTable->patchEntity($entity, $post);
+
+                if ($EntityTable->save($entity)) {
+                    $response = [
+                        'status' => 'success',
+                        'message' => ucfirst($this->getLabel()) . ' editada com sucesso',
+                        'data' => $entity
+                    ];
+                }
+            }
+        }
+
+        $this->set(compact('response'));
+        $this->set('_serialize', ['response']);
+    }
+}
